77746265d1f9cb830ce7d494e47f93d561425ae2
kaaviadharshini <kaaviadharshini@gmail.com>
2024-07-28 15:54:25 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,32 @@
+const inputBox=document.getElementById("input-box");
+const listContainer=document.getElementById("list-container");
+function addTask(){
+    if(inputBox.value===''){
+        alert("Task is empty");
+    }
+    else{
+        let li=document.createElement("li");
+        li.innerHTML=inputBox.value;
+        listContainer.appendChild(li);
+        let span=document.createElement("span");
+        span.innerHTML="\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value="";
+    save();
+}
+listContainer.addEventListener("click",function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+    }
+}, false);
+function save(){
+    localStorage.setItem("data",listContainer.innerHTML)
+}
+function show(){
+    listContainer.innerHTML=localStorage.getItem("data");
+}
+show();
